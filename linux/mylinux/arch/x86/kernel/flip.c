33e043cb89df2f7add0782dc6e423d6b60ff8961
Fujii Yosuke <fujiyou1121@gmail.com>
2017-07-25 11:06:51 +0900

    fix Use copy_from_user & copy_to_user

--- linux/mylinux/arch/x86/kernel/flip.c
+++ linux/mylinux/arch/x86/kernel/flip.c
@@ -6,18 +6,13 @@
 int str_flip(char *p, int len) {
     // Flip given byte
     int i, ret = -1;
-    char tmp[len];
-    for (i=0; i<len; i++) {
-        if (p[i] == '\0') {
-            ret = i;
-            break;
-        }
-        tmp[i] = p[i];
-    }
+    char tmp[len], res[len];
+    unsigned long x = copy_from_user(tmp, p, (unsigned long)len);
     if (ret == -1) ret = len;
     for (i=0; i<ret; i++) {
-        p[i] = tmp[ret-i-1];
+        res[i] = tmp[ret-i-1];
     }
+    unsigned long y = copy_to_user(p, res, (unsigned long)len);
     return ret;
 }
 
